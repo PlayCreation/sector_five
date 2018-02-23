2e2537c1eb4a10250f86d70ec03a7c4674a9715d
chriskottom <chris@chriskottom.com>
2018-02-23 20:45:52 +0100

    Make a ship that moves around the screen

---
+++ player.rb
@@ -0,0 +1,64 @@
+require 'gosu'
+
+class Player
+  ACCELERATION = 2
+  FRICTION = 0.1
+  ROTATION_SPEED = 3
+
+  def initialize(window)
+    @x = 200
+    @y = 200
+    @angle = 0
+
+    @ship_image = Gosu::Image.new('images/ship.png')
+    @thrust_image = Gosu::Image.new('images/ship_thrust.png')
+    @image = @ship_image
+
+    @velocity_x = 0
+    @velocity_y = 0
+    @radius = 20
+    @window = window
+  end
+
+  def draw
+    @image.draw_rot(@x, @y, 1, @angle)
+    @image = @ship_image
+  end
+
+  def turn_right
+    @angle += ROTATION_SPEED
+  end
+
+  def turn_left
+    @angle -= ROTATION_SPEED
+  end
+
+  def move
+    @x += @velocity_x
+    @y += @velocity_y
+    @velocity_x *= (1.0 - FRICTION)
+    @velocity_y *= (1.0 - FRICTION)
+
+    if @x > @window.width - @radius
+      @velocity_x = -@velocity_x
+      @x = @window.width - @radius
+    elsif @x < @radius
+      @velocity_x = -@velocity_x
+      @x = @radius
+    end
+
+    if @y > @window.height - @radius
+      @velocity_y = -@velocity_y
+      @y = @window.height - @radius
+    elsif @y < @radius
+      @velocity_y = -@velocity_y
+      @y = @radius
+    end
+  end
+
+  def accelerate
+    @image = @thrust_image
+    @velocity_x += Gosu.offset_x(@angle, ACCELERATION)
+    @velocity_y += Gosu.offset_y(@angle, ACCELERATION)
+  end
+end
--- sector_five.rb
+++ sector_five.rb
@@ -1,9 +1,28 @@
 require 'gosu'
 
+require_relative './player.rb'
+
 class SectorFive < Gosu::Window
+  WINDOW_HEIGHT = 800
+  WINDOW_WIDTH = 800
+
   def initialize
-    super(800, 600)
+    super(WINDOW_WIDTH, WINDOW_HEIGHT)
     self.caption = 'Sector Five'
+
+    @player = Player.new(self)
+  end
+
+  def draw
+    @player.draw
+  end
+
+  def update
+    @player.turn_left if  button_down?(Gosu::KbLeft)
+    @player.turn_right if  button_down?(Gosu::KbRight)
+
+    @player.accelerate if button_down?(Gosu::KbUp)
+    @player.move
   end
 end
 
